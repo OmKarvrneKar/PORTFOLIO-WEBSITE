0ebda946779cefcfb90d074beb02ed67bbb552ad
OmKarvrneKar <omkarvernekar07@gmail.com>
2026-07-18 01:41:43 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
+  base: '/PORTFOLIO-WEBSITE/',
   plugins: [
     {
       name: 'treat-js-files-as-jsx',
